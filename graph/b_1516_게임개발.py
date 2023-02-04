5f733de7b299fc5ff9d997d16d36feded8cd8179
Matt <matthew.woo@buzzvil.com>
2023-02-04 23:15:20 +0900

    위상정렬 게임개발

---
+++ graph/b_1516_게임개발.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+indegree =[0 for _ in range(n+1)]
+time =[0 for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+answer = [0 for _ in range(n+1)]
+que = deque()
+for i in range(n):
+    input = list(map(int, sys.stdin.readline().split()))
+    indegree[i+1] = len(input)-2
+    time[i+1] = input[0]
+    for j in input[1:-1]:
+        graph[j].append(i+1)
+
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        que.append(i)
+
+while que:
+    now = que.popleft() # B
+    answer[now] += time[now] # B <- B
+        
+    for node in graph[now]:
+        indegree[node] -= 1
+        answer[node] = max(answer[node], answer[now]) 
+        if indegree[node] == 0:
+            que.append(node)
+
+for i in range(1,n+1):
+    print(answer[i])
