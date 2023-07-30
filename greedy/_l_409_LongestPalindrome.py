1a0f7a4ab9df746202e01a013bbacc1947d553fb
Jungbum Woo <jungbumwoo@naver.com>
2023-07-30 22:26:24 +0900

    greedy: 409 LogestPalindrome

---
+++ greedy/_l_409_LongestPalindrome.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+# https://leetcode.com/problems/longest-palindrome
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        # 5분 안에 풀었으니 한번 다시 풀어보기
+
--- greedy/_l_621_task_scheduler.py
+++ greedy/_l_621_task_scheduler.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Tuple
 
 import heapq
 import unittest
-
+import collections
 
 
 #  https://leetcode.com/problems/task-scheduler/
@@ -16,47 +16,6 @@ class Solution:
     def solve(self, tasks: List[str], n: int) -> int:
         # Time Complexity :  
         # Space Complexity : 
-
-        raw = {}
-        for i in range(len(tasks)):
-            if tasks[i] in raw:
-                raw[tasks[i]] += 1
-            else:
-                raw[tasks[i]] = 1
-
-        # print(raw)
-
-        queue = []
-        heapq.heapify(queue)        
-
-        for k, v in raw.items():
-            heapq.heappush(queue, [-v, k])
-
-        empty = 0
-        cnt = 0
-        while queue:
-            # print(queue)
-            temp = []
-            empty = 0
-        
-            for _ in range(n+1):
-                cnt += 1
-                try:
-                    v, k = heapq.heappop(queue)
-                    v = -1 * v
-                    v -= 1
-
-                    if v:
-                        temp.append([-v, k])
-                except:
-                    empty += 1
-                    continue
-        
-            for q in range(len(temp)):
-                heapq.heappush(queue, [temp[q][0], temp[q][1]])
-
-        return cnt - empty
-
         
 # Test
 class TestSolution(unittest.TestCase):
@@ -87,6 +46,26 @@ class TestSolution(unittest.TestCase):
                 name="test 3",
                 input=Args(tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n=2),
                 expect=16
+            ),
+            TestCase(
+                name="test 4",
+                input=Args(tasks = ["A","A","A","B","B","B","C","C","C"], n=2),
+                expect=9
+            ),
+            TestCase(
+                name="test 5",
+                input=Args(tasks = ["A","B","C","D","E"], n=0),
+                expect=5
+            ),
+            TestCase(
+                name="test 6",
+                input=Args(tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n=0),
+                expect=12
+            ),
+            TestCase(
+                name="test 7",
+                input=Args(tasks = ["A","A","A","A","A","A","B","B","B","B","B","B","C","C","C","C","C","C","D","D"], n=2),
+                expect=20
             ),
         ]
 
