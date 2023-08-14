8a8acfc10da834801b998f847ef32f4c3820cbf5
Jungbum Woo <jungbumwoo@naver.com>
2023-08-15 08:54:16 +0900

    dp(knapsack): 평범한 배낭 top-down 추가

--- dp/b_12865_평범한_배낭.py
+++ dp/b_12865_평범한_배낭.py
@@ -1,5 +1,7 @@
 import sys
 
+# https://www.acmicpc.net/problem/12865
+
 N, K = map(int, sys.stdin.readline().split())
 
 '''
@@ -13,7 +15,32 @@ ex input)
 ex ans)
 14
 '''
+# Top-Down
+data = []
+for i in range(N):
+    W, V = map(int, sys.stdin.readline().split())
+    data.append((W, V))
 
+cache = {}
+
+def dfs(i: int, k: int):
+    if i == len(data):
+        return 0
+    
+    if (i, k) in cache:
+        return cache[(i, k)]
+
+    cache[(i, k)] = dfs(i + 1, k)
+    if k - data[i][0] >= 0:
+        cache[(i, k)] = max(dfs(i + 1, k - data[i][0]) + data[i][1], cache[(i, k)])
+
+    return cache[(i, k)]
+
+print(dfs(0, K))
+
+
+# bottom - up
+'''
 data = []
 for i in range(N):
     W, V = map(int, sys.stdin.readline().split())
@@ -30,3 +57,4 @@ for i in range(1, N + 1):
             dp[i][j] = max(v + dp[i-1][j-w], dp[i-1][j], dp[i][j-1])
 
 print(dp[-1][-1])
+'''
