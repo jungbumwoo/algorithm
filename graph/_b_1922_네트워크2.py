c3b21147be45a5d8179a9f648325658bedd6f84e
Jungbum Woo <jungbumwoo@naver.com>
2023-08-28 00:04:16 +0900

    fix: Kruskal 1922 네트워크 연결 - time 개선

--- graph/_b_1922_네트워크2.py
+++ graph/_b_1922_네트워크2.py
@@ -1,7 +1,7 @@
 import heapq
 import sys
 
-# fix: 백준 이슈임. 코드 수정없이 실행하면 또 정답처리됨.
+# fix: 백준 이슈임. 코드 수정없이 실행하면 또 정답처리됨. -> 그게 아니라 처음 틀렸을때 마지막줄 print를 누락하였음..
 
 '''
 input)
@@ -30,10 +30,22 @@ heapq.heapify(queue)
 parent = [i for i in range(N + 1)]
 
 
+'''
+pypy3로는 정답되고 python3로는 시간초과 나던 이슈 해결.
+
+이 문제의 경우에 각 정점마다 parent, child 관계를 남길 필요가 없어서
+매번 재귀로 부모를 찾아가기보다 각 연결된 정점이 직접 연결되지 않는 부모를 직접 참조하는 것이 효율적임.
+
 def find(i):
     if i == parent[i]:
         return i    
     return find(parent[i])
+'''
+
+def find(i):
+    if i != parent[i]:
+        parent[i] = find(parent[i])    
+    return parent[i]
 
 
 def union(x, y):
