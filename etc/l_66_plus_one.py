52ae8543582d6341584503947b28e2d77ea1e89e
Matt <matthew.woo@buzzvil.com>
2023-08-31 22:31:41 +0900

    improve: l_66_plus_one.py

--- etc/l_66_plus_one.py
+++ etc/l_66_plus_one.py
@@ -1,7 +1,6 @@
 # math
 
 
-
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
@@ -15,3 +14,16 @@ class Solution:
             ans.append(int(new_int[j]))
         
         return ans
+    
+    # 좀 더 수학적으로 푼 풀이
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        digits.insert(0, 1)
+        return digits
