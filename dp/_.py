79213030a9c9c68d03579a06397cc8ef7bc2db89
Matt <matthew.woo@buzzvil.com>
2023-09-08 00:54:08 +0900

    fixme: dp: 1965 상자넣기

---
+++ dp/_.py
@@ -0,0 +1,34 @@
+import sys
+
+'''
+5
+1 5 2 3 7
+'''
+
+N = int(sys.stdin.readline())
+
+data = list(map(int, sys.stdin.readline().rstrip().split()))
+cache = {}
+
+def select(index, weight):
+    if index == len(data):
+        return 0
+    
+    if (index, weight) in cache:
+        return cache[(index, weight)]
+
+    a, b = 0, 0
+    if data[index] < weight:
+        # selected
+        a = select(index + 1, data[index]) + 1
+
+    # not selected
+    b = select(index + 1, weight)
+
+    ans = max(a, b)
+    cache[(index, weight)] = ans
+    return ans
+
+# print(select(0, 0))
+# print(select(0, 10 ** 7))
+print()
