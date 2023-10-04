2de598bc89118a027f301041cc5df821c27cf008
matthew.woo <jungbumwoo@naver.com>
2023-10-05 07:03:26 +0900

    template apply to l_424

--- sliding_window/_l_424_Longest Repeating Character Replacement.py
+++ sliding_window/_l_424_Longest Repeating Character Replacement.py
@@ -1,6 +1,22 @@
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+import unittest
+
+
+# https://leetcode.com/problems/longest-repeating-character-replacement/
+
+'''
+You are given a string s and an integer k. 
+You can choose any character of the string and change it to any other uppercase English character. 
+You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+'''
+
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        
+    def solve(self, s: str, k: int) -> List[int]:
+        # Time Complexity :  
+        # Space Complexity : 
         inital_window_size = k + 1 # 바꾸는 수 (k) + 최소 하나 (1)
 
         l, r = 0, 0 + inital_window_size - 1
@@ -38,9 +54,53 @@ class Solution:
 
 
         for i in range(0, search_end_point):
+            print(f"i: {i}, window_size: {window_size}")
             new_window_size = search(i, window_size, k)
             while new_window_size > window_size:
                 window_size = new_window_size
+                print(f"i: {i}, window_size: {window_size}")
                 new_window_size = search(i, window_size, k)
                 
         return window_size
+    
+        
+# Test
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        @dataclass
+        class Args:
+            s: str
+            k: int
+
+        @dataclass
+        class TestCase:
+            name: str
+            input: Args
+            expect: int
+
+        cases = [
+            TestCase(
+                name="test 1",
+                input=Args(s = "AABABBA", k=1),
+                expect=4
+            ),
+            TestCase(
+                name="test 2",
+                input=Args(s = "ABAB", k=2),
+                expect=4
+            ),
+        ]
+
+        solution = Solution()
+        for c in cases:
+            actual = solution.solve(s=c.input.s, k=c.input.k)
+
+            self.assertEqual(
+                c.expect,
+                actual,
+                f"failed test {c.name} expected {c.expect}, actual {actual}"
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
