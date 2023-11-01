6000fc36154c1756356b1ca5918949e98ff17399
matthew.woo <jungbumwoo@naver.com>
2023-11-01 09:02:08 +0900

    fixme: l_665_non_decreasing_array

---
+++ array/l_665_Non-decreasing_Array.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+import unittest
+
+
+# https://leetcode.com/problems/non-decreasing-array/
+
+'''
+Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+'''
+
+class Solution:
+    def solve(self, nums: List[int]) -> bool:
+        # Time Complexity : O(n) 
+        # Space Complexity : O(1)
+        if len(nums) == 1:
+            return True
+
+        already = False
+        for i in range(1, len(nums)):
+            if nums[i-1] > nums[i]:
+                if already is False:
+                    already = True
+                elif already is True:
+                    return False
+        return True
+
+        
+# Test
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        @dataclass
+        class Args:
+            nums: List[int]
+
+        @dataclass
+        class TestCase:
+            name: str
+            input: Args
+            expect: int
+
+        cases = [
+            TestCase(
+                name="test 1",
+                input=Args(nums = [4,2,3]),
+                expect=True,
+            ),
+            TestCase(
+                name="test 2",
+                input=Args(nums = [4,2,1]),
+                expect=False
+            ),
+            TestCase(
+                name="test 3",
+                input=Args(nums = [3,4,2,3]),
+                expect=False
+            ),
+        ]
+
+        solution = Solution()
+        for c in cases:
+            actual = solution.solve(nums=c.input.nums)
+
+            self.assertEqual(
+                c.expect,
+                actual,
+                f"failed test {c.name} expected {c.expect}, actual {actual}"
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
