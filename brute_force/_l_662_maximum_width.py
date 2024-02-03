8a79b5bac0ce75dbf01f30fbd40786a22b2fb4cd
jungbumwoo <jungbumwoo@naver.com>
2024-02-03 18:05:56 +0900

    _l_662_maximum_width

--- brute_force/_l_662_maximum_width.py
+++ brute_force/_l_662_maximum_width.py
@@ -30,6 +30,7 @@ class Solution:
         return ans
 
 # fixme 2.
+# error case: [1,1,1,1,1,1,1,null,null,null,1,null,null,null,null,2,2,2,2,2,2,2,null,2,null,null,2,null,2]
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         
@@ -50,4 +51,53 @@ class Solution:
         
         search(root, 0, 1)
         return ans[0]
+    
+# fixme 3. 
+# error case: [1]
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        data = {}
+        ans = [0]
+
+        def search(node, level, index):
+            if node is None:
+                return
+            
+            if level not in data:
+                data[level] = index
+            else:
+                ans[0] = max(ans[0], index - data[level] + 1)
+            
+            search(node.left, level + 1, 2 * index - 1)
+            search(node.right, level + 1, 2 * index)
+        
+        search(root, 0, 1)
+        print(data)
+        return ans[0]
+
+# solved            
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        data = {}
+        ans = [0]
+
+        def search(node, level, index):
+            if node is None:
+                return
+            
+            if level not in data:
+                data[level] = index
+                ans[0] = max(ans[0], 1)
+            else:
+                ans[0] = max(ans[0], index - data[level] + 1)
+            
+            search(node.left, level + 1, 2 * index - 1)
+            search(node.right, level + 1, 2 * index)
+        
+        search(root, 0, 1)
+        print(data)
+        return ans[0]
+            
                         
