0a7aced4c9fed8c38ded92bf679e5003e86bf127
jungbumwoo <jungbumwoo@naver.com>
2024-03-01 17:30:39 +0900

    _l_93_Restore_IP_Address

---
+++ backtracking/_93_Restore_IP_Addresses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        stack = []
+        ans = []
+
+        def select(s, i, n):
+            if n == 5:
+                ans.append(".".join(stack))
+                return
+            if i >= len(s):
+                return
+
+            for k in range(1,4):
+                if i + k > len(s):
+                    continue
+
+                str_num = s[i:i+k]
+                int_num = int(str_num)
+
+                if int_num > 255:
+                    continue
+                if len(str_num) != len(str(int_num)):
+                    continue
+                
+                stack.append(str_num)
+                select(s, i+k, n + 1)
+                stack.pop()
+        
+        select(s, 0, 1)
+        return ans
