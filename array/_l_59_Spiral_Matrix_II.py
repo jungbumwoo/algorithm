87704d002670defdf76b91008a266328194daa8c
jungbumwoo <jungbumwoo@naver.com>
2024-03-17 23:25:52 +0900

    _l_59_Spiral_Matrix_II.py

---
+++ array/_l_59_Spiral_Matrix_II.py
@@ -0,0 +1,27 @@
+class Solution:
+    direction = 0
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    def get_next_direction(self):
+        self.direction = (self.direction + 1) % 4
+        return self.direction
+
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        result = [[0] * n for _ in range(n)]
+
+        x, y = 0, 0
+        for i in range(1, n+1):
+
+            result[x][y] = i
+
+            nx = x + self.dx[self.direction]
+            ny = y + self.dy[self.direction]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n or result[nx][ny] > 0:
+                new_direction = self.get_next_direction()
+                nx, ny = x + self.dx[new_direction], y + self.dy[new_direction]
+            
+            x, y = nx, ny
+
+        return result
