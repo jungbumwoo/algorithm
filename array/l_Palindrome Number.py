1453360a6700fcd5993f55ba1e8ba6c52f46dd3f
jungbumwoo <jungbumwoo@naver.com>
2024-03-31 21:58:09 +0900

    l_Palindrome number

---
+++ array/l_Palindrome Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+
+        inputNum = x
+        newNum = 0
+
+        while x>0:
+            newNum = newNum * 10 + x%10
+            x = x//10
+
+            print(f"newNum: {newNum}")
+            print(f"x: {x}")
+
+        return newNum == inputNum
