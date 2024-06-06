4044fabd3746381ad16c49769dcf89f4389f7ad1
jungbumwoo <jungbumwoo@naver.com>
2024-06-07 07:00:15 +0900

    fix 120 triangle

--- array/_l_120_Triangle.py
+++ array/_l_120_Triangle.py
@@ -1,20 +1,24 @@
 # https://leetcode.com/problems/triangle
 
+'''
+cache를 class variable로 뒀더니 테스트 케이스간에 공유되는 문제가 있었다.
+functools.cache로 해결하여 변경함
+
+Bottom Up 으로 풀어볼 수도 있음.
+'''
+
+import functools
+
 class Solution:
-    cache = {}
 
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         self.triangle = triangle
         return self.search(0, 0)
 
-    
+    @functools.cache
     def search(self, i, j):
         if i == len(self.triangle) or j == len(self.triangle) or j < 0:
             return 0
 
-        if (i, j) in self.cache:
-            return self.cache[(i, j)]
+        return self.triangle[i][j] + min(self.search(i+1, j), self.search(i+1, j+1))
         
-        ans = self.triangle[i][j] + min(self.search(i+1, j), self.search(i+1, j+1))
-        self.cache[(i, j)] = ans
-        return ans
