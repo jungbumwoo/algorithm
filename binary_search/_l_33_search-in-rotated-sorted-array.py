a846328d76f20aaf777f2b88f32231d70364b9c1
jungbumwoo <jungbumwoo@naver.com>
2024-07-14 22:30:07 +0900

    l 33 search in rotated sorted array

--- binary_search/_l_33_search-in-rotated-sorted-array.py
+++ binary_search/_l_33_search-in-rotated-sorted-array.py
@@ -75,3 +75,46 @@ class Solution:
             return -1
 
         return ans
+    
+
+'''
+다시 풀어봄.
+재귀함수 쓰지 않고 풀이를 더 개선할 여지가 남았음.
+
+다른 사람 풀이:
+https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/14425/concise-o-log-n-binary-search-solution/
+
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self._search(0, len(nums)-1, nums, target)
+    
+    def _search(self, left, right, nums, target):
+        if left == right:
+            if nums[left] == target:
+                return left
+            else:
+                return -1
+
+        if nums[left] > nums[right]:
+            mid = (left + right) // 2
+            l = self._search(left=left, right=mid, nums=nums, target=target)
+            r = self._search(left=mid+1, right=right, nums=nums, target=target)
+
+            if l != -1:
+                return l
+            if r != -1:
+                return r
+            return -1
+        
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
