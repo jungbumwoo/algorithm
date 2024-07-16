5b2847705709324e6583b55917a239c7e6382db2
jungbumwoo <jungbumwoo@naver.com>
2024-07-16 21:45:30 +0900

    _l_888

---
+++ binary_search/_l_888_Fair_Candy_Swap.py
@@ -0,0 +1,26 @@
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        
+        aliceSizes = sorted(aliceSizes)
+        bobSizes = sorted(bobSizes)
+        alice_sum = sum(aliceSizes)
+        bob_sum = sum(bobSizes)
+
+        for i in range(len(aliceSizes)):
+            # target = bob_sum - (alice_sum - aliceSizes[i])
+            # alice_sum - aliceSizes[i] + target = bob_sum + aliceSizes[i] - target
+            target = (bob_sum - alice_sum + 2 * aliceSizes[i]) // 2
+            left, right = 0, len(bobSizes) - 1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if bobSizes[mid] == target:
+                    return [aliceSizes[i], target]
+                elif bobSizes[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        raise ValueError('Unexpected case')
+
