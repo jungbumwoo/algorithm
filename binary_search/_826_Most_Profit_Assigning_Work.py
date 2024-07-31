4b313919cfa042d9480cd1704e97644be3afab85
jungbumwoo <jungbumwoo@naver.com>
2024-07-31 22:58:42 +0900

    fix 826

---
+++ binary_search/_826_Most_Profit_Assigning_Work.py
@@ -0,0 +1,32 @@
+# fixme
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        data = []
+        for i in range(len(difficulty)):
+            data.append((difficulty[i], profit[i]))
+        
+        data.sort()
+
+        cache = [0] * len(data)
+        cache[0] = data[0][1]
+        for j in range(1, len(data)):
+            cache[j] = max(cache[j-1], data[j][1])
+        
+        result = 0
+        for w in worker:
+            result += self.find(w, difficulty, cache)
+
+        return result
+    
+    def find(self, w, difficulty, cache):
+        left, right = 0, len(difficulty)
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            if w >= difficulty[mid]:
+                left = mid + 1
+            else:
+                right = mid
+        return cache[left]
+                
