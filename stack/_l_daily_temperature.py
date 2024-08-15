1a716a0b913f325165404f492669677f835aedcc
jungbumwoo <jungbumwoo@naver.com>
2024-08-16 08:16:48 +0900

    fix: _l_739_daily_temperature

--- stack/_l_daily_temperature.py
+++ stack/_l_daily_temperature.py
@@ -4,19 +4,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         ans = [0] * len(temperatures)
+        MAX_ABOVE = 10 ** 5
         if len(temperatures) == 1:
             return ans
 
-        ex_index = 0
-        stack_top = 0
+        ex_index = [0]
+        stack_top = [MAX_ABOVE]
         for i in range(len(temperatures)):
             temp = temperatures[i]
+            if temp > stack_top[-1]:
+                while temp > stack_top[-1]:
+                    ans[ex_index[-1]] = i - ex_index[-1]
+                    ex_index.pop()
+                    stack_top.pop()
+            ex_index.append(i)
+            stack_top.append(temp)
 
-            if temp > stack_top:
-                while ex_index < i:
-                    ans[ex_index] = i - ex_index
-                    ex_index += 1
-                
-                stack_top = temp
-                ex_index = i
         return ans
